5b1b124ee0c49e67712e584f3a11e6f0c3974e0e
noah <noah@shpakee.com>
2015-08-10 13:46:18 -0600

    Basic file structure, manually added three items from feed

---
+++ DoYouBelieve/client/templates/posts_list.js
@@ -0,0 +1,17 @@
+var postsData = [
+  {
+    title: 'July 31: Wisconsin Budget, Taxes, Climate Change',
+    url: 'http://www.factcheck.org/2015/07/july-31-wisconsin-budget-taxes-climate-change/'
+  },
+  {
+    title: 'Mailbag: Planned Parenthood Videos',
+    url: 'http://www.factcheck.org/2015/08/mailbag-planned-parenthood-videos/'
+  },
+  {
+    title: 'FactChecking the GOP Debate, Late Edition',
+    url: 'http://www.factcheck.org/2015/08/factchecking-the-gop-debate-late-edition/'
+  }
+];
+Template.postsList.helpers({
+  posts: postsData
+});
