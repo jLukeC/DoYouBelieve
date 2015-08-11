df1e23dc36a8e68df28531b24c098fdacdd70d33
Luke Carlson <jlukecarlson@gmail.com>
2015-08-11 12:36:02 -0400

    added users and responses

--- DoYouBelieve/client/templates/statements_list.js
+++ DoYouBelieve/client/templates/statements_list.js
@@ -3,3 +3,16 @@ Template.statementsList.helpers({
         return Statements.find();
     }
 });
+// TODO - should this just be in statement_item?
+Template.statementsList.events({
+    'click .true': function(event) {
+        event.preventDefault();
+        // 'this' is the statement object
+        // TODO - remove insecure
+        Responses.insert({
+            sid: this._id,
+            uid: Meteor.userId(),
+            res: true
+        });
+    }
+});
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # DoYouBelieve
 Test how often you are fooled by political candidates' statements
+
+
+
+# DB
+Users - users on the site
+Statements - A collection of statements made by politicians
+Responses - How users responded to a statement (T/F) {_id,sid,uid,res}.
+
+Responses is its own collection so we can quickly determine responses when we only have _id of the related statement (sid) or when we just have the user id who made the response (uid)
